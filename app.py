8d8f576f9e347b514dd48e57e44a39e2724d8e36
Shaganti Nandhitha <145770245+SHAGANTI-NANDHITHA@users.noreply.github.com>
2024-12-18 17:24:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,39 +5,17 @@ import numpy as np
 import os
 import requests
 import tensorflow as tf  # Import TensorFlow
+from io import BytesIO
 
-# Function to download and load the model from Google Drive
-@st.cache_resource
-def load_model_from_drive():
-    try:
-        # Direct download link for the model file
-        url = "https://drive.google.com/uc?id=1ZuZCCybYsXt4c1F1WYNjuG0cszeGp3UA"
-        model_path = "skin_cancer_model.h5"
+# Function to download the model file from Google Drive
+def download_model():
+    model_url = "https://drive.google.com/uc?id=1UumJIfM8xtDzKUTLKNE6drHlVnaboUAu"
+    response = requests.get(model_url)
+    model = tf.keras.models.load_model(BytesIO(response.content))
+    return model
 
-        # Check if the model is already downloaded
-        if not os.path.exists(model_path):
-            # Download the model file
-            st.info("Downloading the model file. Please wait...")
-            response = requests.get(url, stream=True)
-            if response.status_code != 200:
-                st.error("Failed to download the model. Please check the link.")
-                return None
-
-            # Save the downloaded model file
-            with open(model_path, "wb") as f:
-                for chunk in response.iter_content(chunk_size=1024):
-                    if chunk:
-                        f.write(chunk)
-
-        # Load the model
-        model = tf.keras.models.load_model(model_path)
-        st.success("Model loaded successfully!")
-        return model
-
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
-        return None
-
+# Load the model
+model = download_model()
 
 # Define class labels
 class_labels = ['Benign', 'Malignant']
@@ -45,9 +23,6 @@ class_labels = ['Benign', 'Malignant']
 # Streamlit App
 st.title("Skin Cancer Classification")
 st.write("Upload an image to classify it as benign or malignant.")
-
-# Load the model
-model = load_model_from_drive()
 
 # File uploader
 uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
